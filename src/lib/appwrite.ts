8861958ce1ec2d17b0adf5ca42e6e8d3bd6155ab
anjula-sack <anjulashanaka@gmail.com>
2025-12-14 08:42:27 +0530

    Remove project URL

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -2,7 +2,7 @@ import { AssessmentRecord, TeacherInfo } from "@/types";
 import { Account, Client, Databases, Query, Storage } from "appwrite";
 
 const client = new Client()
-  .setEndpoint("https://fra.cloud.appwrite.io/v1")
+  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || "")
   .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || "");
 
 export const account = new Account(client);
